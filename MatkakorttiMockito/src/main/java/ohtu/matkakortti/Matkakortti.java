5bf06b40acd80c36700b736d71f5ea72c647ecb3
Sami Lindqvist <sami.lindqvist@helsinki.fi>
2017-11-20 22:20:52 +0200

    3 done

---
+++ MatkakorttiMockito/src/main/java/ohtu/matkakortti/Matkakortti.java
@@ -0,0 +1,26 @@
+package ohtu.matkakortti;
+
+public class Matkakortti {
+
+    private int saldo;
+
+    public Matkakortti(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void lataa(int maara) {
+        saldo += maara;
+    }
+
+    public void osta(int hinta) {
+        if (hinta > getSaldo()) {
+
+        } else {
+            saldo -= hinta;
+        }
+    }
+}
